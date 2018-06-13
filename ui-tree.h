2575ed363fb8bc19a0e4227de0860c677fb21287
John Keeping <john@keeping.me.uk>
2018-06-13 10:01:55 +0800

    ui-tree: use render fileters to display content
    This allows applying filters to files in the repository, for example to
    render Markdown or AsciiDoc as HTML.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- ui-tree.h
+++ ui-tree.h
@@ -1,6 +1,6 @@
 #ifndef UI_TREE_H
 #define UI_TREE_H
 
-extern void cgit_print_tree(const char *rev, char *path);
+extern void cgit_print_tree(const char *rev, char *path, bool use_render);
 
 #endif /* UI_TREE_H */
